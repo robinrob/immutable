4d8ef0e297669700e867abab1d54c2f24478596e
Robin Smith <robin@rsmith.io>
2018-05-29 13:36:17 +0100

    Simplify install script

--- install_startup_sound_script.zsh
+++ install_startup_sound_script.zsh
@@ -1,10 +1,7 @@
 #!/usr/bin/env zsh
 
-# Configure plist file with current user's home directory
-/usr/bin/sed -i '' s/username/$(/usr/bin/id -un)/ com.rsmithio.disable_startup_sound.plist
+# Configure plist file with current user's home directory and output into user LaunchAgents directory
+/usr/bin/sed s/username/$(/usr/bin/id -un)/ com.rsmithio.disable_startup_sound.plist > $HOME/Library/LaunchAgents/com.rsmithio.disable_startup_sound.plist
 
 # Copy daemon script into home directory to make it easy to reference from plist file
 cp disable_startup_sound.zsh $HOME/.disable_startup_sound.zsh
-
-# Copy daeomon plist file into user LaunchAgents directory
-cp com.rsmithio.disable_startup_sound.plist $HOME/Library/LaunchAgents/
